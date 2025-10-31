8ea94a8e7a9fa61179708a992ee076a3b346379b
s4158210 <s4158210@student.rmit.edu.au>
2025-10-31 13:47:16 +1100

    test again for wc3 validation

--- a3/instructor.php
+++ a3/instructor.php
@@ -1,14 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
 // instructor.php — Bootstrap layout (no custom <style>), schema-safe
 
